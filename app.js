4aff8af3828c89cb5437f2ffca73ae09b98c8042
A-Abd <abrar.abdulwahed@gmail.com>
2022-02-17 00:20:26 +0300

    update configure of deploye

--- app.js
+++ app.js
@@ -3,18 +3,9 @@ const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 3000;
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Hello World</h1>');
-  });
-  
-server.listen(port,() => {
-    console.log(`Server running at port `+port);
-});
 app.set('view engine','ejs');
-
-
+app.listen(port);
+app.use(express.static('public'));
 
 app.use(express.static('public'));
 app.get("/", (req, res)=>{
