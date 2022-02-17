3454ea76b7dcc0a5161e8073d11dc537058970bd
A-Abd <abrar.abdulwahed@gmail.com>
2022-02-17 13:43:46 +0300

    add 404 page

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ app.listen(port);
 app.use(express.static('public'));
 
 app.use(express.static('public'));
-app.get("/", (req, res)=>{
+app.get(["/", "/home", "index"], (req, res)=>{
     res.render("index");
+});
+app.use((req, res) => {
+    res.status(404).render('404');
 });
