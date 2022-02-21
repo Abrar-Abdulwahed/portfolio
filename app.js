0fabd1571651b1853fdea1d991deeaaae82d9149
A-Abd <abrar.abdulwahed@gmail.com>
2022-02-22 00:37:37 +0300

    add dashboard

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.set('view engine','ejs');
 app.listen(port);
 app.use(express.static('public'));
 
-app.use(express.static('public'));
+
 app.get(['/', '/home', '/index'], (req, res)=>{
     res.render('index', {title: 'Abrar - Portfolio'});
 });
